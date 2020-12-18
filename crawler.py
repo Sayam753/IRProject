2a2831dc3bf0381f6f7c0b3452306cd23d58b5fd
Raahul Singh <raahulsingh002@gmail.com>
2020-12-18 17:02:46 +0530

    Updates the paper to only consider Influential papers

--- crawler.py
+++ crawler.py
@@ -29,6 +29,8 @@ class ArXivPaper():
         self.check_relevant_keys()
         self.discard_non_influential_citations()
         self.discard_non_influential_references()
+        self.discard_none_arxiv_references()
+        self.discard_none_arxiv_citations()
 
     def check_paper(self):
         if isinstance(self.paper, str):
@@ -51,6 +53,12 @@ class ArXivPaper():
 
     def discard_non_influential_citations(self):
         self.paper['citations'] = list(filter(lambda i: i['isInfluential'] is True, self.paper['citations']))
+
+    def discard_none_arxiv_references(self):
+        self.paper['references'] = list(filter(lambda i: i['arxivId'] is not None, self.paper['references']))
+
+    def discard_none_arxiv_citations(self):
+        self.paper['citations'] = list(filter(lambda i: i['arxivId'] is not None, self.paper['citations']))
 
     def discard_non_influential_references(self):
         self.paper['references'] = list(filter(lambda i: i['isInfluential'] is True, self.paper['references']))
@@ -80,7 +88,7 @@ class ArXivPaper():
              warnings.warn(f"Total citations are {self.__getitem__('numCitations')}. Retrieving all citations")
              k = self.__getitem__('numCitations')
 
-        citations = {}
+        citations = []
         all_citations = self.__getitem__('citations')
 
         info_keys = ['arxivId', 'authors', 'title', 'url','venue', 'year']
@@ -88,14 +96,8 @@ class ArXivPaper():
         i=0
         while i < k:
             citation = all_citations[i]
-            if citation['arxivId'] is None:
-                warnings.warn(f"The citation at index {i+1} has no Arxiv ID. Skipping this citation.")
-                k+=1
-                i+=1
-                continue
-
             citation = {key:val for key, val in citation.items() if key in info_keys}
-            citations[i+1] = citation
+            citations.append(citation)
             i+=1
 
         return citations
@@ -105,7 +107,7 @@ class ArXivPaper():
              warnings.warn(f"Total references are {self.__getitem__('numReferences')}. Retrieving all references")
              k = self.__getitem__('numReferences')
 
-        references = {}
+        references = []
         all_references = self.__getitem__('references')
 
         info_keys = ['arxivId', 'authors', 'title', 'url','venue', 'year']
@@ -113,34 +115,27 @@ class ArXivPaper():
         i=0
         while i < k:
             reference = all_references[i]
-
-            if reference['arxivId'] is None:
-                warnings.warn(f"The reference at index {i+1} has no Arxiv ID. Skipping this reference.")
-                k+=1
-                i+=1
-                continue
-
             reference = {key:val for key, val in reference.items() if key in info_keys}
-            references[i+1] = reference
+            references.append(reference)
             i+=1
 
         return references
 
     def get_top_k_references_metadata(self, k:int):
-        reference_papers = {}
+        reference_papers = []
         references = self.get_top_k_references_information(k)
 
-        for i in range(1, len(references)+1):
-            reference_papers[i] = ArXivPaper(references[i]['arxivId'])
+        for i in range(len(references)):
+            reference_papers.append(ArXivPaper(references[i]['arxivId']))
 
         return reference_papers
 
     def get_top_k_citations_metadata(self, k:int):
-        citation_papers = {}
-        citations = self.get_top_k_references_information(k)
+        citation_papers = []
+        citations = self.get_top_k_citations_information(k)
 
-        for i in range(1, len(citations)+1):
-            citation_papers[i] = ArXivPaper(citations[i]['arxivId'])
+        for i in range(len(citations)):
+            citation_papers.append(ArXivPaper(citations[i]['arxivId']))
 
         return citation_papers
 
